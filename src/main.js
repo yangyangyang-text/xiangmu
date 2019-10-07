33d5d97c42015f90c4a10f4e09efd6db72f0fb15
yangyangyang-text <335971864@qq.com>
2019-10-07 20:22:33 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/theme-chalk/index.css' // 引入样式
+import './style/index.less' //  引入初始化样式
 Vue.config.productionTip = false
 Vue.use(ElementUI) // 全局注册
 new Vue({
